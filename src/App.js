321f8cd414348fd59be227cd159a46965c637b4a
Singularity2050 <67400401+Singularity2050@users.noreply.github.com>
2023-03-04 00:34:04 -0500

    basic
    basic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import * as React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import Home from "./Pages/Home"
+
+
+
+
 
 function App() {
+    //Login
+    //Registration
+    //Main Page
+    
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path={"/"} element={<Home/>}/>
+          {/*<Route path={"/register"} element={<Register/>} />*/}
+          {/*<Route path={"/profile"} element={<Profile />} />*/}
+          {/*<Route path={"/home"} element={<Home />} />*/}
+        </Routes>
+      </BrowserRouter>
   );
 }
 
---
+++ src/Pages/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import NavBottom from "../Components/NavBottom";
+import NavTop from "../Components/NavTop";
+import Camera from "../Components/Camara";
+
+const Home = () => {
+    return (
+        <>
+            <NavTop/>
+            <Camera />
+            <NavBottom/>
+        </>
+    );
+};
+
+export default Home;
+
+
